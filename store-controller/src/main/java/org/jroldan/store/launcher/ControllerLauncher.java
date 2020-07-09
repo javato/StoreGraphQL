3f4763d03003a03a8c6b2eaee3d8c1b634a84143
javirolga <javirolga@gmail.com>
2020-07-09 21:14:38 +0000

    added spring data dependency + scan config

--- store-controller/src/main/java/org/jroldan/store/launcher/ControllerLauncher.java
+++ store-controller/src/main/java/org/jroldan/store/launcher/ControllerLauncher.java
@@ -2,8 +2,14 @@ package org.jroldan.store.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.jroldan.store.*"})
+@EntityScan(basePackages = {"org.jroldan.store.*"})
+@EnableJpaRepositories(basePackages = {"org.jroldan.store.*"})
 public class ControllerLauncher {
 
 	public static void main(String[] args) {

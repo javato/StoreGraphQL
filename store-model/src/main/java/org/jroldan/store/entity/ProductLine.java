9df3333db35c8fe77dd6dfa295074740ad9aa9c2
javirolga <javirolga@gmail.com>
2020-07-12 15:19:00 +0000

    fix mapping with productLines

--- store-model/src/main/java/org/jroldan/store/entity/ProductLine.java
+++ store-model/src/main/java/org/jroldan/store/entity/ProductLine.java
@@ -3,8 +3,8 @@ package org.jroldan.store.entity;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 @Entity
@@ -14,25 +14,12 @@ public class ProductLine {
 	@EmbeddedId
 	private ProductLineId id;
 	
-	@ManyToOne
-	@MapsId("idProduct")
+	@ManyToOne(optional = false)
+	@JoinColumn(name="id_product", insertable = false, updatable = false)
 	private Product product;
-	
-	@ManyToOne
-	@MapsId("idOrder")
-	private Order order;
 	
 	@Column
 	private Long quantity;
-	
-	
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
 
 	public Long getQuantity() {
 		return quantity;
@@ -50,11 +37,4 @@ public class ProductLine {
 		this.id = id;
 	}
 
-	public Order getOrder() {
-		return order;
-	}
-
-	public void setOrder(Order order) {
-		this.order = order;
-	}
 }
--- store-model/src/main/java/org/jroldan/store/entity/ProductLineId.java
+++ store-model/src/main/java/org/jroldan/store/entity/ProductLineId.java
@@ -1,7 +1,6 @@
 package org.jroldan.store.entity;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -18,7 +17,6 @@ public class ProductLineId implements Serializable{
 	private Long idProduct;
 	
 	
-
 	public ProductLineId() {
 		
 	}
@@ -27,23 +25,37 @@ public class ProductLineId implements Serializable{
 		this.idOrder = idOrder;
 		this.idProduct = idProduct;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idOrder == null) ? 0 : idOrder.hashCode());
+		result = prime * result + ((idProduct == null) ? 0 : idProduct.hashCode());
+		return result;
+	}
 
 	@Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
- 
-        if (o == null || getClass() != o.getClass())
-            return false;
- 
-        ProductLineId that = (ProductLineId) o;
-        return Objects.equals(idOrder, that.idOrder) &&
-               Objects.equals(idProduct, that.idProduct);
-    }
- 
-    @Override
-    public int hashCode() {
-        return Objects.hash(idOrder, idProduct);
-    }
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductLineId other = (ProductLineId) obj;
+		if (idOrder == null) {
+			if (other.idOrder != null)
+				return false;
+		} else if (!idOrder.equals(other.idOrder))
+			return false;
+		if (idProduct == null) {
+			if (other.idProduct != null)
+				return false;
+		} else if (!idProduct.equals(other.idProduct))
+			return false;
+		return true;
+	}
 
 	public Long getIdOrder() {
 		return idOrder;
@@ -60,5 +72,5 @@ public class ProductLineId implements Serializable{
 	public void setIdProduct(Long idProduct) {
 		this.idProduct = idProduct;
 	}
-	
+
 }
